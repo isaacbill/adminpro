06aadfaea3a06fc7a147062b336044d41ffbd8dc
Ian-Mark <ianarapkirui@gmail.com>
2023-07-25 16:19:55 +0300

    Product page modal update 2

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -9,43 +9,39 @@ const columns: GridColDef[] = [
     field: "name",
     type: "string",
     headerName: "Name",
-    width: 200,
+    width: 250,
   },
   {
     field: "price",
     type: "number",
     headerName: "Price",
-    width: 200,
+    width: 250,
   },
   {
     field: "quantity",
     headerName: "Quantity",
     type: "string",
-    width: 200,
+    width: 250,
   },
   {
     field: "descritption",
     headerName: "Description",
     type: "string",
-    width: 200,
+    width: 250,
   },
-
-  // {
-  //   field: "createdAt",
-  //   headerName: "Created At",
-  //   width: 200,
-  //   type: "string",
-  // },
-  // {
-  //   field: "inStock",
-  //   headerName: "In Stock",
-  //   width: 150,
-  //   type: "boolean",
-  // },
+];
+const categColumns: GridColDef[] = [
+  {
+    field: "name",
+    type: "string",
+    headerName: "Name",
+    width: 100,
+  },
 ];
 
 const Products = () => {
   const [open, setOpen] = useState(false);
+  const [categOpen, setCategOpen] = useState(false);
   return (
     <div className="products">
       <div className="container">
@@ -54,7 +50,9 @@ const Products = () => {
             <input type="search" className="prodSearch" />
           </div>
           <div className="right">
-            <button className="addCateg">+ Category</button>
+            <button onClick={() => setCategOpen(true)} className="addCateg">
+              + Category
+            </button>
             <button onClick={() => setOpen(true)} className="addProd">
               + Product
             </button>
@@ -67,6 +65,9 @@ const Products = () => {
         </div>
       </div>
       {open && <Add slug="product" columns={columns} setOpen={setOpen} />}
+      {categOpen && (
+        <Add slug="category" columns={categColumns} setOpen={setCategOpen} />
+      )}
     </div>
   );
 };
