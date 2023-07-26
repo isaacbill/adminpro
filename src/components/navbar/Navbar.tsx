9a086d9bead7c96752f081d58ff6cc08556fa0e5
isaac bill <isaacbill247@gmail,com>
2023-07-26 16:23:26 +0300

    reactJs frontend

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,7 +4,8 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="logo">
-        <img src="logo.svg" alt="" />
+        <img src="productboard.svg" alt="" />
+        <span>ProductBoard</span>
       </div>
       <div className="icons">
         {/* <img src="/search.svg" alt="" className="icon" />
@@ -15,7 +16,7 @@ const Navbar = () => {
           <span>1</span>
         </div> */}
         <div className="user">
-          <span>Mark</span>
+          <span>Isaac</span>
           <img
             src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
             alt=""
